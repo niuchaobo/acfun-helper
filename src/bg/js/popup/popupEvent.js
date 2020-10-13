075c9392885730db000e310c4bafec541a44534c
Sokwva <1519181530@qq.com>
2020-10-13 21:02:06 +0800

    禁用掉usercard涉及的模块功能

--- src/bg/js/popup/popupEvent.js
+++ src/bg/js/popup/popupEvent.js
@@ -209,119 +209,100 @@ export async function userInfoFetch() {
 	fetch("https://www.acfun.cn/rest/pc-direct/user/userInfo?userId=" + Number(uid)).then((res) => {
 		if (res.status == 503) {
 			chrome.runtime.sendMessage({ action: 'notice', params: { title: "AcFun助手", msg: "您操作太过频繁。" } }, function (response) { });
-		}
-		return res.text()
+		} return res.text()
 	})
 		.then((res) => {
 			let x = JSON.parse(res);
 			if (x.result != 0) { chrome.runtime.sendMessage({ action: 'notice', params: { title: "AcFun助手", msg: "UID可能存在着某些问题。" } }, function (response) { }); return }
 			if (x.profile.contentCount != 0) { dougaCountFlag = 1 }
-			fetch("https://www.acfun.cn/usercard.aspx?uid=" + Number(uid)).then((res2) => { return res2.text() })
-				.then((res2) => {
-					let ExtraInfo = JSON.parse(res2);
-					let raw_data = `
-      <div class="mdui-table-fluid">
-          <table class="mdui-table">
-              <thead>
-              <tr>
-                  <th>#</th>
-                  <th>Data</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                  <td>UID</td>
-                  <td>${x.profile.userId}</td>
-              </tr>
-              <tr>
-                  <td>用户名</td>
-                  <td>${x.profile.name}</td>
-              </tr>
-              <tr>
-                  <td>注册于</td>
-                  <td>${getTimeSinceNow(x.profile.registerTime, true)}</td>
-              </tr>
-              <tr>
-                  <td>签名</td>
-                  <td>${(x.profile.signature)}</td>
-              </tr>
-              <tr>
-                  <td>认证</td>
-                  <td>${(ExtraInfo.userjson.verifiedText)} - ${x.profile.verifiedTypes.indexOf(1) != -1 ? "猴子" : ""} - ${x.profile.isContractUp ? "学院Up" : ""}</td>
-              </tr>
-              <tr>
-                  <td>关注</td>
-                  <td>${x.profile.following}</td>
-              </tr>
-              <tr>
-                  <td>粉丝</td>
-                  <td>${x.profile.followed} 或者说 ${ExtraInfo.userjson.fans}个</td>
-              </tr>
-              <tr>
-                  <td>稿件数</td>
-                  <td>${x.profile.contentCount}</td>
-              </tr>
-              <tr>
-                  <td>所有评论</td>
-                  <td>${ExtraInfo.userjson.comments}</td>
-              </tr>
-              <tr>
-                  <td>所有收藏</td>
-                  <td>${ExtraInfo.userjson.stows}</td>
-              </tr>
-              <tr>
-                  <td>所有观看</td>
-                  <td>${ExtraInfo.userjson.views}</td>
-              </tr>
-              </tbody>
-          </table>
-          </div>
-      `;
-					$("#UserInfoPrint").append(raw_data);
-					if (dougaCountFlag != 1) { return }
-					fetch(`https://api-new.app.acfun.cn/rest/app/user/resource/query?count=1&authorId=${Number(uid)}&resourceType=2&sortType=3`).then((res) => { return res.text() })
-						.then((res) => {
-							let x = JSON.parse(res);
-							let raw_data = `
-        <div class="mdui-table-fluid">
-            <table class="mdui-table">
-                <thead>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>上次视频投于</td>
-                    <td>${getTimeSinceNow(x.feed[0].createTimeMillis, true)}</td>
-                </tr>
-                </tbody>
-            </table>
-            </div>
-        `;
-							$("#UserInfoPrint").append(raw_data);
-						})
-					fetch(`https://api-new.app.acfun.cn/rest/app/user/resource/query?count=1&authorId=${Number(uid)}&resourceType=3&sortType=3`).then((res) => {
-						return res.text()
-					})
-						.then((res) => {
-							let x = JSON.parse(res);
-							if (x.feed[0].contributeTime == undefined) { return }
-							let raw_data = `
-        <div class="mdui-table-fluid">
-            <table class="mdui-table">
-                <thead>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>上次文章投于</td>
-                    <td>${getTimeSinceNow(x.feed[0].contributeTime, true)}</td>
-                </tr>
-                </tbody>
-            </table>
-            </div>
-        `;
-							$("#UserInfoPrint").append(raw_data);
-						})
-				})
-		})
+			let raw_data = `
+		<div class="mdui-table-fluid">
+			<table class="mdui-table">
+				<thead>
+				<tr>
+					<th>#</th>
+					<th>Data</th>
+				</tr>
+				</thead>
+				<tbody>
+				<tr>
+					<td>UID</td>
+					<td>${x.profile.userId}</td>
+				</tr>
+				<tr>
+					<td>用户名</td>
+					<td>${x.profile.name}</td>
+				</tr>
+				<tr>
+					<td>注册于</td>
+					<td>${getTimeSinceNow(x.profile.registerTime, true)}</td>
+				</tr>
+				<tr>
+					<td>签名</td>
+					<td>${(x.profile.signature)}</td>
+				</tr>
+				<tr>
+					<td>认证</td>
+					<td>${x.profile.verifiedTypes.indexOf(1) != -1 ? "猴子" : ""} - ${x.profile.isContractUp ? "学院Up" : ""}</td>
+				</tr>
+				<tr>
+					<td>关注</td>
+					<td>${x.profile.following}</td>
+				</tr>
+				<tr>
+					<td>粉丝</td>
+					<td>${x.profile.followed}</td>
+				</tr>
+				<tr>
+					<td>稿件数</td>
+					<td>${x.profile.contentCount}</td>
+				</tr>
+				</tbody>
+			</table>
+			</div>
+			`;
+			$("#UserInfoPrint").append(raw_data);
+			if (dougaCountFlag != 1) { return }
+			fetch(`https://api-new.app.acfun.cn/rest/app/user/resource/query?count=1&authorId=${Number(uid)}&resourceType=2&sortType=3`).then((res) => { return res.text() })
+			.then((res) => {
+				let x = JSON.parse(res);
+				let raw_data = `
+				<div class="mdui-table-fluid">
+					<table class="mdui-table">
+						<thead>
+						</thead>
+						<tbody>
+						<tr>
+							<td>上次视频投于</td>
+							<td>${getTimeSinceNow(x.feed[0].createTimeMillis, true)}</td>
+						</tr>
+						</tbody>
+					</table>
+					</div>
+				`;
+				$("#UserInfoPrint").append(raw_data);
+			})
+			fetch(`https://api-new.app.acfun.cn/rest/app/user/resource/query?count=1&authorId=${Number(uid)}&resourceType=3&sortType=3`).then((res) => { return res.text() })
+			.then((res) => {
+				let x = JSON.parse(res);
+				if (x.feed[0].contributeTime == undefined) { return }
+				let raw_data = `
+			<div class="mdui-table-fluid">
+				<table class="mdui-table">
+					<thead>
+					</thead>
+					<tbody>
+					<tr>
+						<td>上次文章投于</td>
+						<td>${getTimeSinceNow(x.feed[0].contributeTime, true)}</td>
+					</tr>
+					</tbody>
+				</table>
+				</div>
+			`;
+				$("#UserInfoPrint").append(raw_data);
+			})
+	})
 
 }
 
