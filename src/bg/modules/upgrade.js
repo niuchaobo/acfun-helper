6daabd660e5f159b22cd616cf38d19e46bc95a29
Sokwva <1519181530@qq.com>
2021-04-22 16:55:15 +0800

    Backend增加Alarms及其钩子、upgrade模块改为alarm

--- src/bg/modules/upgrade.js
+++ src/bg/modules/upgrade.js
@@ -6,9 +6,9 @@ class UpgradeAgent {
     constructor() {
         this.ModuleName = 'UpgradeAgent';
         this.checkConfigDay = [3, 7]
-        this.dateTimer = null;
         this.bangumiPlan = new BangumiPlan();
         this.option = '';
+        this.scheduler = myBrowser() == "Chrome" ? chrome.alarms : browser.alarms;
     }
 
     /**
@@ -51,29 +51,24 @@ class UpgradeAgent {
     async upgradeMain() {
         console.log("Registered Upgrade Check Mod.");
         // 配置获取
-        let x = await getStorage("krnl_globalTimer");
-        this.dateTimer = x.krnl_globalTimer;
-        let x1 = await getStorage("BangumiNotif");
-        let BangumiNotifsw = x1.BangumiNotif;
-        let x2 = await getStorage("BangumiPlan");
-        let BangumiPlansw = x2.BangumiPlan;
-        // 启动项
+        let x = await getStorage("krnl_globalTimer").then(function (e) {
+            return e.krnl_globalTimer;
+        })
+        if (x) {
+            this.scheduleTasks();
+        }
+        // 定时执行（一天的样子）
+        this.scheduler.create("scheduleTasks", { "periodInMinutes": 1440 })
+    }
+
+    async scheduleTasks() {
+        //配置
+        let BangumiNotifsw = await getStorage("BangumiNotif").then(function (e) { return e.BangumiNotif });
+        let BangumiPlansw = await getStorage("BangumiPlan").then(function (e) { return e.BangumiPlan });
+
+        //调用
         this.checkUpdate();
-        this.bangumiPlan.onLoad();
         BangumiNotifsw && this.bangumiPlan.fetchBangumiInfo();
         BangumiPlansw && this.bangumiPlan.notifyBangumiUpdate();
-        // 定时执行（一天的样子）
-        this.dateTimer && clearInterval(this.dateTimer);
-        this.dateTimer = setInterval(async () => {
-            // 定时器内部配置获取
-            let x1 = await getStorage("BangumiNotif");
-            let BangumiNotifsw = x1.BangumiNotif;
-            let x2 = await getStorage("BangumiPlan");
-            let BangumiPlansw = x2.BangumiPlan;
-            //调用
-            this.checkUpdate();
-            BangumiNotifsw && this.bangumiPlan.fetchBangumiInfo();
-            BangumiPlansw && this.bangumiPlan.notifyBangumiUpdate();
-        }, 43200000);
     }
 }
