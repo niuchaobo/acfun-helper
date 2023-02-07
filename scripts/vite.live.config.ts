64ddc279fe55d5eca99d2d074bcb3477a0a6b086
Sokwva <1519181530@qq.com>
2023-02-07 13:26:10 +0800

    chore: vite with hmr

--- scripts/vite.live.config.ts
+++ scripts/vite.live.config.ts
@@ -4,5 +4,11 @@ import { crx } from "@crxjs/vite-plugin";
 import manifest from "./crxManifest.json" assert { type: "json" };
 
 export default defineConfig({
+  server: {
+    cors: true,
+    hmr: {
+      port: 5174,
+    },
+  },
   plugins: [vue(), crx({ manifest })],
 });
--- src/UI/Popup/popup.ts
+++ src/UI/Popup/popup.ts
@@ -1,1 +1,2 @@
 console.log("Here");
+export {};
