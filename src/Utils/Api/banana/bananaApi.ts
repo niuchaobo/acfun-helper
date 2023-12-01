394d73133397247f540784a7c7d06750389ea497
Sokwva <1519181530@qq.com>
2023-12-01 23:30:32 +0800

    feat: add banana Api

---
+++ src/Utils/Api/banana/bananaApi.ts
@@ -0,0 +1,31 @@
+import { fetchResult } from "@/Core/CoreUtilLibs/WebRequest"
+
+export const banana = {
+    throw: "https://www.acfun.cn/rest/pc-direct/banana/throwBanana",
+}
+
+enum ApiDougaType {
+    Video = 2,
+    Article = 3
+}
+
+export interface throwResponse {
+    result: number
+    extData: {
+        bananaRealCount: number
+        criticalHitInfo: string
+    }
+}
+
+export async function doThrow(resourceId: string, banana_num: number, dougaType: string = "video") {
+    if (banana_num > 5 && banana_num < 1) {
+        throw new Error("invalid banana_num.")
+    }
+    let resType = ApiDougaType.Video;
+    if (dougaType == "article") {
+        resType = ApiDougaType.Article;
+    }
+
+    let data = "resourceId=" + resourceId + "&count=" + banana_num + "&resourceType=" + resType;
+    return fetchResult(banana.throw, "POST", data, "same-origin", "cors","default","strict-origin-when-cross-origin",window.location.href);
+}
