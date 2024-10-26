89fecfcaa5e791917ee8af44d98ca11de97cffe0
Sokwva <1519181530@qq.com>
2024-10-26 23:54:54 +0800

    chore: add usage MsgRouter.MsgClient

--- src/UI/Popup/popup.ts
+++ src/UI/Popup/popup.ts
@@ -1,2 +1,9 @@
-console.log("Here");
-export {};
+import { MessageRouter } from "@/Core/CoreLibs/MessageRouter";
+
+(async ()=>{
+    const MsgClient = new MessageRouter.MsgClient({ module: chrome.runtime });
+    const EchoMsg = await MsgClient.send("/bg/echo", { "data": "Hi There" })
+    console.log(EchoMsg)
+})
+
+export { };
