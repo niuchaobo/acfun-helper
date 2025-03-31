5d237f464b3b14c0ff5ff1ca7981290ede451c34
Sokwva <1519181530@qq.com>
2025-03-31 22:30:03 +0800

    fix: LiveDanmuTime DOM.addEventListener DOMNodeInserted Deprecated

---
+++ src/Modules/Live/liveDanmuTime.ts
@@ -0,0 +1,50 @@
+import { ModuleStd } from "@/Declare/FeatureModule";
+import { ExtOptions, GetAsyncDOM } from "@/Core/CoreUtils";
+import { modLog } from "@/Core/CoreLibs/ConsoleProxy";
+import { isTargetPage, REG } from "@/Core/Regs";
+import { DOMObserverSlim } from "@/Core/CoreLibs/DOMObserver";
+
+interface Conf {
+    enable: boolean
+}
+
+let allOptions: Conf;
+
+const main = async () => {
+    allOptions = await ExtOptions.getValue(module.name) as Conf;
+    if (!allOptions.enable) {
+        return
+    }
+
+    if (!isTargetPage(REG.live)) {
+        return
+    }
+    modLog("Init", module.name, "main")
+
+    GetAsyncDOM.Get(".live-feed-messages", () => {
+        const livefeedArea = document.querySelector(".live-feed-messages");
+        if (!livefeedArea) {
+            return
+        }
+        DOMObserverSlim.L1Cilds(livefeedArea, e => {
+            const targetElem = e[0].target as HTMLElement;
+            const time = new Date();
+            const firstChild = targetElem.children[0].firstChild;
+            const span = document.createElement("span");
+            span.innerText = `[${time.toLocaleTimeString()}]`;
+            targetElem.children[0].insertBefore(span, firstChild);
+        })
+    })
+}
+
+export const defaultConf: Conf = {
+    enable: true
+}
+
+export const module: ModuleStd.manifest = {
+    name: "LiveDanmuTime",
+    type: ModuleStd.ModType.Hybrid,
+    workSpace: ModuleStd.WorkSpace.Frontend,
+    sequentialType: ModuleStd.SequentialType.Loaded,
+    main
+}
