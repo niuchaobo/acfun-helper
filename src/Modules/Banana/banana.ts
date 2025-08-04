c1e44dad15a18a18cd24af0f2f2f90c8ee2dcd8e
Sokwva <1519181530@qq.com>
2025-08-04 23:42:50 +0800

    feat: add Banana

--- src/Modules/Banana/banana.ts
+++ src/Modules/Banana/banana.ts
@@ -1,16 +1,49 @@
 import { modLog } from "@/Core/CoreLibs/ConsoleProxy";
-import { REG, isTargetPage, pageAcID } from "@/Core/Regs";
+import { REG, isTargetPage, pageAcID, FgBroadcastChannelName } from "@/Core/Regs";
 import { ModuleStd } from "@/Declare/FeatureModule";
 import { hasThrowBanana, isFollowed, isLogin } from "@/Utils/Check";
 import { getUpUid } from "@/Utils/Get";
 import { bananaThrow } from "./libs/throw";
+import { ExtOptions } from "@/Core/CoreUtils";
+
+interface Conf {
+    enable: boolean
+    mode: "all" | "followed" | "specific"
+    allModeBanaNum: number
+    followModeBanaNum: number
+    specificModeWithFollowed: boolean
+    userMapBananaNum: Record<string, number>
+}
+
+let allOptions: Conf;
+
+export const defaultConf: Conf = {
+    enable: true,
+    mode: "followed",
+    allModeBanaNum: 5,
+    followModeBanaNum: 0,
+    specificModeWithFollowed: false,
+    userMapBananaNum: {
+
+    }
+}
+
 
 //所有Up 关注的Up 特定的Up 关注的和特定的Up
 const main = async () => {
+    allOptions = await ExtOptions.getValue(module.name) as Conf;
+    if (!allOptions.enable) {
+        return
+    }
+    const MsgSw = new BroadcastChannel(FgBroadcastChannelName);
+    const mode = allOptions.mode;
+    if (mode == undefined) {
+        return modLog("mode not set in config")
+    }
     //是否是正确的页面
     let isVideo: boolean, isArticle: boolean
-    isVideo = isTargetPage(REG.video)
-    isArticle = isTargetPage(REG.article)
+    isVideo = isTargetPage(REG.video);
+    isArticle = isTargetPage(REG.article);
     if (!isVideo && !isArticle) {
         return
     }
@@ -19,7 +52,7 @@ const main = async () => {
         return
     }
     //是否已投
-    if (!await hasThrowBanana) {
+    if (await hasThrowBanana(isVideo ? "video" : "article")) {
         return
     }
     //获取Up的Uid
@@ -27,21 +60,45 @@ const main = async () => {
     //是否已关注
     let hasFollowed = isVideo ? await isFollowed("video") : await isFollowed("article");
     //获取AcID
-    let acID = "";
-    let vids;
-    if (isVideo) {
-        vids = pageAcID(REG.acVid);
-    } else {
-        vids = pageAcID(REG.acAid);
-    }
-    if (vids == null) {
+    let vids = pageAcID();
+    if (vids == null || vids == undefined) {
         modLog("no acid found", module.name, "main");
         return;
-    } else {
-        acID = vids[2];
     }
-    modLog(Uid + " ;hasFollowed: " + hasFollowed, module.name, "main");
 
+    switch (mode) {
+        case "all":
+            bananaThrow(vids, allOptions.allModeBanaNum, isVideo ? "video" : "article");
+            break;
+        case "followed":
+            hasFollowed && bananaThrow(vids, allOptions.allModeBanaNum, isVideo ? "video" : "article");
+            if (!allOptions.specificModeWithFollowed) {
+                break;
+            }
+        case "specific":
+            let requireBananaNum = allOptions.userMapBananaNum[Uid]
+            if (typeof (requireBananaNum) != "number") {
+                modLog("unvalid userMapBanana of " + Uid);
+                return
+            }
+            if (requireBananaNum < 1 || requireBananaNum > 6) {
+                bananaThrow(vids, allOptions.userMapBananaNum[Uid], isVideo ? "video" : "article")
+            }
+        default:
+            break;
+    }
+}
+
+const confUpdateDict: string[] = []
+const confUpdateFunc: CallableFunction[] = []
+
+const updateTrigger = (type: string, ...args: any[]) => {
+    switch (type) {
+        case "conf":
+            return ExtOptions.updateConf(confUpdateDict, confUpdateFunc, args)
+        default:
+            break;
+    }
 }
 
 export const module: ModuleStd.manifest = {
@@ -49,5 +106,6 @@ export const module: ModuleStd.manifest = {
     type: ModuleStd.ModType.FucntionCode,
     workSpace: ModuleStd.WorkSpace.Frontend,
     sequentialType: ModuleStd.SequentialType.Loaded,
+    updateTrigger,
     main,
 }
--- src/Modules/Banana/libs/throw.ts
+++ src/Modules/Banana/libs/throw.ts
@@ -1,4 +1,6 @@
+import { modLog } from "@/Core/CoreLibs/ConsoleProxy";
 import { doThrow, throwResponse } from "@/Utils/Api/banana/bananaApi";
+import { GetAsyncDOM } from "@/Core/CoreUtils";
 
 /**
  * 投蕉
@@ -9,9 +11,38 @@ export async function bananaThrow(resourceId: string, banana_num: number, dougaT
     let result = await doThrow(resourceId, banana_num, dougaType)
     if (!!result) {
         res_obj = JSON.parse(result);
-        if (res_obj.result == 0) {
-            return true
+        switch (res_obj.result) {
+            case 0:
+                modLog(`投喂稿件${resourceId} ${banana_num}根香蕉ﾟ∀ﾟ)σ`);
+                pageBananaState(banana_num, dougaType);
+                return true
+            case 170004:
+                modLog(`没有足够的香蕉可以投喂了( ﾟ∀。)`);
+                return false
         }
     }
     return false
+}
+
+export function pageBananaState(banana_num: number, type: string) {
+    if (!banana_num) {
+        return;
+    }
+    const bananaCountDom = document.querySelector(".bananaCount") as HTMLElement;
+    const JBa_numDom = document.querySelectorAll(".Jba_num") as NodeListOf<HTMLElement>;
+    if (bananaCountDom == null || JBa_numDom == null) {
+        return
+    }
+
+    if (type == "video") {
+        $('.right-area .banana').addClass('active');
+        bananaCountDom.innerText = String(Number(bananaCountDom.innerText) + Number(banana_num));
+    } else if (type == "article") {
+        GetAsyncDOM.Get("div.census-right", () => {
+            document.querySelectorAll('.bananacount')[0].classList.add("active");
+            document.querySelectorAll('.bananacount')[1].classList.add("active");
+            JBa_numDom[0].innerText = String(Number(JBa_numDom[0].innerText) + Number(banana_num));
+            JBa_numDom[2].innerText = String(Number(JBa_numDom[2].innerText) + Number(banana_num));
+        })
+    }
 }
